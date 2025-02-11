b2dc30a2fb267993d47fd7b598b12d4785d9fa56
akp956 <akhilpadhy999@gmail.com>
2025-02-11 15:19:12 +0000

    j

--- JS_BASICS/datatypes.js
+++ JS_BASICS/datatypes.js
@@ -10,5 +10,13 @@ boolean
 undefined
 null=>standalone value
 
+primitive datatypes are 7:number,string,boolean,undefined,symbol,null,bigint
+non primitive (reference):array,objects,functions
+stack(primitive)--gives copy and heap(non primitive)--gives reference
 */ 
 //console.log(typeof number)
+
+
+
+
+
