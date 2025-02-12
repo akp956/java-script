c1c201ae8fe2703ded241adf879099ba71a16f39
akp956 <akhilpadhy999@gmail.com>
2025-02-12 13:54:08 +0000

    math and num

---
+++ JS_BASICS/numsandmath.js
@@ -0,0 +1,16 @@
+const num=100
+const number=new Number(5554544521.12)
+console.log(num)
+console.log(number)
+console.log(number.toString())
+console.log(number.toFixed(3))//decimals
+console.log(number.toPrecision(2))//roundoff
+console.log(number.toLocaleString('en-IN'))//commas
+//MATHS
+console.log(Math)
+console.log(Math.abs(-9))
+console.log(Math.round(2.3))
+console.log(Math.ceil(2.3))
+console.log(Math.floor(2.3))
+console.log(Math.random())//gives values between 0 and 1
+console.log(Math.floor(Math.random()*10)+1)
---
+++ JS_BASICS/strings.js
@@ -0,0 +1,21 @@
+const name="hitesh"
+const repocount=20
+console.log(`my name is ${name} and repocount is ${repocount}`)//string interpolation using backticks
+const  myname=new String("akhil")
+// console.log(myname)
+// console.log(myname[0])
+// console.log(myname.length)
+// console.log(myname.toUpperCase())
+// console.log(myname.charAt(2 ))
+// console.log(myname.indexOf('a'))
+// console.log(myname.substring(0,2))
+// const neww=myname.slice(-3,1)
+// console.log(neww)
+let text = "Hello world!";
+let result = text.slice(0,3);
+console.log(result)
+//trim
+//replace
+//includes
+//split(seperator ,limit)converts into array
+//use MDN for more functions
